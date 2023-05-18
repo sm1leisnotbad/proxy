17ca958ae4387f3a2db2ca4ab6be448e07778d55
Huy Tran <tnh0087@gmail.com>
2023-05-18 14:10:02 +0700

    them function

--- proxy/proxy/Program.cs
+++ proxy/proxy/Program.cs
@@ -23,6 +23,11 @@ namespace proxy
 
         NetworkStream clientStream;
         NetworkStream serverStream;
+
+        AsymmetricCipherKeyPair fakeKeyPair;
+        ECPublicKeyParameters fakePublicKey;
+        ECPrivateKeyParameters fakePrivateKey;
+
         static void Main(string[] args)
         {
             Program program = new Program();
@@ -43,6 +48,52 @@ namespace proxy
 
         }
 
+        void GetClientPublicKey()
+        {
+            byte[] fakePublicKeyInByte = fakePublicKey.Q.GetEncoded();
+            clientStream.Write(fakePublicKeyInByte, 0, fakePublicKeyInByte.Length);
+
+            byte[] realPublicKeyClientInByte = new byte[fakePublicKeyInByte.Length];
+            clientStream.Read(realPublicKeyClientInByte, 0, realPublicKeyClientInByte.Length);
+
+
+            //save secret
+            ECPublicKeyParameters RealClientPublicKey = new ECPublicKeyParameters(
+                "ECDSA",
+                fakePublicKey.Parameters.Curve.DecodePoint(realPublicKeyClientInByte),
+                fakePublicKey.Parameters
+                 );
+            IBasicAgreement agree = AgreementUtilities.GetBasicAgreement("ECDH");
+            agree.Init(fakePrivateKey);
+            byte[] ClientSecret = agree.CalculateAgreement(RealClientPublicKey).ToByteArray();
+            Console.WriteLine("Client Secret: ", Convert.ToBase64String(ClientSecret));
+            File.WriteAllText("client_secret.txt", Convert.ToBase64String(ClientSecret));
+
+        }
+
+        void GetServerPublicKey()
+        {
+            byte[] fakePublicKeyInByte = fakePublicKey.Q.GetEncoded();
+            serverStream.Write(fakePublicKeyInByte, 0, fakePublicKeyInByte.Length);
+
+            byte[] realPublicKeyServerInByte = new byte[fakePublicKeyInByte.Length];
+            clientStream.Read(realPublicKeyServerInByte, 0, realPublicKeyServerInByte.Length);
+
+
+            //save secret
+            ECPublicKeyParameters RealSeverPublicKey = new ECPublicKeyParameters(
+                "ECDSA",
+                fakePublicKey.Parameters.Curve.DecodePoint(realPublicKeyServerInByte),
+                fakePublicKey.Parameters
+            );
+            IBasicAgreement agree = AgreementUtilities.GetBasicAgreement("ECDH");
+            agree.Init(fakePrivateKey);
+
+            byte[] ServerSecret = agree.CalculateAgreement(RealSeverPublicKey).ToByteArray();
+            Console.WriteLine("Server secret: ", Convert.ToBase64String(ServerSecret));
+            File.WriteAllText("sever_secret.txt", Convert.ToBase64String(ServerSecret));
+        }
+
         void MITM()
         {
             //Receive cureve name from server and send to client
@@ -55,7 +106,7 @@ namespace proxy
             Console.WriteLine("Proxy: start ECDHE in " + curvename);
             serverStream.Flush();
             clientStream.Write(curNameBytes, 0, curNameBytes.Length);
-            
+
 
             //Calculate fake public key and send to client and server
             X9ECParameters curveParam = SecNamedCurves.GetByName(curvename);
@@ -63,144 +114,11 @@ namespace proxy
             ECKeyGenerationParameters keyGenParam = new ECKeyGenerationParameters(curve, new SecureRandom());
             ECKeyPairGenerator gen = new ECKeyPairGenerator();
             gen.Init(keyGenParam);
-            AsymmetricCipherKeyPair fakeClientKeyPair = gen.GenerateKeyPair();
 
-/*            ECPublicKeyParameters fakePublicKeyClient = (ECPublicKeyParameters)fakeClientKeyPair.Public;
-            ECPrivateKeyParameters fakePrivateKeyClient = (ECPrivateKeyParameters)fakeClientKeyPair.Private;*/
-
-            AsymmetricCipherKeyPair fakeKeyPair = gen.GenerateKeyPair();
-            ECPublicKeyParameters fakePublicKey = (ECPublicKeyParameters)fakeKeyPair.Public;
-            ECPrivateKeyParameters fakePrivateKey = (ECPrivateKeyParameters)fakeKeyPair.Private;
-
-            byte[] fakePublicKeyInByte = fakePublicKey.Q.GetEncoded();
-            serverStream.Write(fakePublicKeyInByte, 0, fakePublicKeyInByte.Length);
-            clientStream.Write(fakePublicKeyInByte, 0, fakePublicKeyInByte.Length);
-
-            //Receive real public key from client and server
-            byte[] realPublicKeyClientInByte = new byte[fakePublicKeyInByte.Length];
-            serverStream.Read(realPublicKeyClientInByte, 0, realPublicKeyClientInByte.Length);
-
-            byte[] realPublicKeyServerInByte = new byte[fakePublicKeyInByte.Length];
-            clientStream.Read(realPublicKeyServerInByte, 0, realPublicKeyServerInByte.Length);
-
-
-
-            ECPublicKeyParameters RealSeverPublicKey = new ECPublicKeyParameters(
-                "ECDSA",
-                fakePublicKey.Parameters.Curve.DecodePoint(realPublicKeyServerInByte),
-                fakePublicKey.Parameters
-                );
-            IBasicAgreement agree = AgreementUtilities.GetBasicAgreement("ECDH");
-            agree.Init(fakePrivateKey);
-
-            byte[] ServerSecret = agree.CalculateAgreement(RealSeverPublicKey).ToByteArray();
-            Console.WriteLine("Server secret: ", Convert.ToBase64String(ServerSecret));
-            File.WriteAllText("sever_secret.txt", Convert.ToBase64String(ServerSecret));
-
-
-            ECPublicKeyParameters RealClientPublicKey = new ECPublicKeyParameters(
-                "ECDSA",
-                fakePublicKey.Parameters.Curve.DecodePoint(realPublicKeyClientInByte),
-                fakePublicKey.Parameters
-                );
-            agree = AgreementUtilities.GetBasicAgreement("ECDH");
-            agree.Init(fakePrivateKey);
-            byte[] ClientSecret = agree.CalculateAgreement(RealClientPublicKey).ToByteArray();
-            Console.WriteLine("Client Secret: ", Convert.ToBase64String(ClientSecret));
-            File.WriteAllText("client_secret.txt", Convert.ToBase64String(ClientSecret));
-
-
-
-
-/*
-            ECPublicKeyParameters RealClientPublicKey = new ECPublicKeyParameters(
-                "ECDSA",
-                fakePublicKeyClient.Parameters.Curve.DecodePoint(realPublicKeyClientInByte),
-                fakePublicKeyClient.Parameters
-                );
-
-            agree = AgreementUtilities.GetBasicAgreement("ECDH");
-            agree.Init(fakePrivateKey);
-            byte[] RealClientPublicKeyInByte = agree.CalculateAgreement(RealSeverPublicKey).ToByteArray();
-            Console.WriteLine("Proxy send to server message: ", Convert.ToBase64String(RealSeverPublicKeyInByte));
-            File.WriteAllText("public-key_sever.txt", Convert.ToBase64String(RealSeverPublicKeyInByte));
-
-
-
-
-
-            byte[] fake2Server = fakePublicKeyClient.Q.GetEncoded();
-            serverStream.Write(fake2Server, 0, fake2Server.Length);
-
-            byte[] getFromServer = new byte[fakePublicKeyClient.Q.GetEncoded().Length];
-            serverStream.Read(getFromServer, 0, getFromServer.Length);
-
-            Console.WriteLine("Received Public Key from server");
-
-
-            ECPublicKeyParameters severPublicKey = new ECPublicKeyParameters (
-                "ECDSA", 
-                fakePublicKeyClient.Parameters.Curve.DecodePoint(getFromServer), 
-                fakePublicKeyClient.Parameters
-                );
-
-            IBasicAgreement agree = AgreementUtilities.GetBasicAgreement("ECDH");
-            agree.Init(fakePrivateKeyClient);
-
-            byte[] sharedSecret = agree.CalculateAgreement(severPublicKey).ToByteArray();
-
-            Console.WriteLine("Proxy send to server message: ", Convert.ToBase64String(sharedSecret));
-            File.WriteAllText("public-key_sever.txt", Convert.ToBase64String(sharedSecret));
-
-
-            //////////////////////////////
-            /// client
-            //////////////////////////////
-            ///
-
-            clientStream.Write(Encoding.ASCII.GetBytes(curvename), 0, curvename.Length);
-
-            curve = new ECDomainParameters(curveParam);
-
-            keyGenParam = new ECKeyGenerationParameters(curve, new SecureRandom());
-            gen = new ECKeyPairGenerator();
-
-            gen.Init(keyGenParam);
-            AsymmetricCipherKeyPair fakeServerKeyPair = gen.GenerateKeyPair();
-
-            ECPublicKeyParameters fakePublicKeyServer = (ECPublicKeyParameters)fakeServerKeyPair.Public;
-            ECPrivateKeyParameters fakePrivateKeyServer = (ECPrivateKeyParameters)fakeServerKeyPair.Private;    
-
-            byte[] fakeKey2Client = fakePublicKeyServer.Q.GetEncoded();
-            clientStream.Write(fakeKey2Client, 0, fakeKey2Client.Length);
-
-            byte[] RealClientdata = new byte[fakeKey2Client.Length];
-            clientStream.Read(RealClientdata, 0, RealClientdata.Length);
-
-            Console.WriteLine("Received client public key");
-
-            ECPublicKeyParameters RealClientPublicKey = new ECPublicKeyParameters(
-                "ECDSA",
-                fakePublicKeyServer.Parameters.Curve.DecodePoint(RealClientdata),
-                fakePublicKeyServer.Parameters
-                );
-
-            agree = AgreementUtilities.GetBasicAgreement("ECDH");
-            agree.Init(fakePrivateKeyServer);
-
-
-            Array.Clear(sharedSecret, 0, sharedSecret.Length);
-
-
-            sharedSecret = agree.CalculateAgreement(severPublicKey).ToByteArray();
-            Console.WriteLine("Proxy send to server message: ", Convert.ToBase64String(sharedSecret));
-            File.WriteAllText("public-key_sever.txt", Convert.ToBase64String(sharedSecret));
-*/
-
-
+            fakeKeyPair = gen.GenerateKeyPair();
+            fakePublicKey = (ECPublicKeyParameters)fakeKeyPair.Public;
+            fakePrivateKey = (ECPrivateKeyParameters)fakeKeyPair.Private;
 
         }
-
-
     }
 }
